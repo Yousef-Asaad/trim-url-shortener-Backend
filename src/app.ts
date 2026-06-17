97229f39550ce2db2c24880bf44e3c252d9badfd
Yousef-Asaad <92746043+Yousef-Asaad@users.noreply.github.com>
2026-06-17 10:54:05 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,10 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({ status: "Backend is running" });
+});
+
 app.use("/", linkRoutes);
 
 app.use(errorHandler);
